c18bfb9341702678a344650eda564fe6b639a3ed
deandum28 <dumitrud@uindy.edu>
2017-05-16 20:43:36 -0500

    Update
    New repo. All changes moved over.

---
+++ Assets/MinionMathMayhem/Scripts/Ship2/GameControllerMinion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MinionMathMayhem_Ship
+{
+	public class GameControllerMinion : MonoBehaviour {
+
+		private int number;
+		public Text numText;
+
+		private void Awake()
+		{
+			numText = GetComponentInChildren<Text>();
+		}
+			
+		private void Start()
+		{
+			//number = Minion_RandomSetNumbers.Access_GetNumber();
+			number = Random.Range(0,13);
+			numText.text = number.ToString();
+		}
+		public int MinionNumber
+		{
+			get {
+				return number;
+			} // get
+		} // MinionNumber
+	}
+}
---
+++ Assets/TheTreadmill/Scripts/Canvas/BoardDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BoardDisplay : MonoBehaviour 
+{
+    GameControl gameController = new GameControl(); //Get current Game info
+
+	// Update is called once per frame
+	public void UpdateBoard () 
+    {
+        GameObject.Find("Lives_Text").GetComponent<Text>().text = "" + gameController.GetLives();
+        GameObject.Find("Lives_Text").GetComponent<Animator>().SetTrigger("Play");
+
+        Invoke("UpdateRounds", 2);
+    }
+
+    public void UpdateRounds()
+    {
+        GameObject.Find("Rounds_Text").GetComponent<Text>().text = gameController.GetCurrentRound() + " / 12";
+        GameObject.Find("Rounds_Text").GetComponent<Animator>().SetTrigger("Play");
+    }
+}

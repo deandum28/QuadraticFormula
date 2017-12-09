5d91d9613e0f30562bbd44b0589b4aff38fa32da
ChrisDrech92 <drechslerc@uindy.edu>
2017-12-09 16:16:12 -0500

    Update Level 1
    Ship is now rotating when complexity increases

---
+++ Assets/MinionMathMayhem/Scripts/Ship/AI/AI_ComplexityRotate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AI_ComplexityRotate : MonoBehaviour {
+
+
+	public Text value;
+	public GameObject ship;
+
+	private Animator ani;
+
+	private string holder;
+
+
+	void Awake(){
+		ani = GetComponent<Animator> ();
+	}
+
+	void Start(){
+		holder = value.text;
+	}
+
+	void Update() {
+		if (holder != value.text) {
+			if (value.text.EndsWith ("0")) { return;
+			} else {
+				StartCoroutine (Rotation ());
+				holder = value.text;
+			}
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			StartCoroutine (Rotation ());
+		}
+		
+	}
+
+	IEnumerator Rotation(){
+		yield return new WaitForSeconds (4f);
+		ani.enabled = false;
+	
+		iTween.RotateBy (ship, iTween.Hash ("z", 0.05, "easeType", "easeInOutExpo", "delay", .1));
+		iTween.RotateBy (ship, iTween.Hash ("z", -0.05, "easeType", "easeInOutExpo", "delay", 1.5));
+
+		yield return new WaitForSeconds (2.5f);
+
+		ani.enabled = true;
+	}
+		
+}
